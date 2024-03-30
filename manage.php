de5391115d2cc8882947c8660c5172162e448181
katiue <vinhkhhangnguyen38@gmail.com>
2024-03-30 12:33:19 +0700

    enhance function

--- manage.php
+++ manage.php
@@ -1,11 +1,18 @@
 <?php
 // Include the database connection file
 include_once "settings.php";
+function sanitise_input($data)
+{
+    $data = trim($data);
+    $data = stripcslashes($data);
+    $data = htmlspecialchars($data);
+    return $data;
+}
 
 // Check if the form is submitted for listing all EOIs
 if(isset($_POST['UPDATE'])){
-    $update_value=$_POST['status'];
-    $update_application=$_POST['EOI'];
+    $update_value=sanitise_input($_POST['status']);
+    $update_application=sanitise_input($_POST['EOI']);
     $sql= "UPDATE EOI SET status = '$update_value' WHERE EOInumber = $update_application";
     $result = mysqli_query($conn, $sql);
 }
@@ -18,7 +25,7 @@ if(isset($_POST['list_all'])) {
 
 // Check if the form is submitted for listing EOIs by job reference number
 if(isset($_POST['list_by_reference'])) {
-    $job_reference = $_POST['job_reference'];
+    $job_reference = sanitise_input($_POST['job_reference']);
     $sql = "SELECT * FROM EOI WHERE job_reference = '$job_reference'";
     $result = mysqli_query($conn, $sql);
     // Display the results
@@ -26,8 +33,8 @@ if(isset($_POST['list_by_reference'])) {
 
 // Check if the form is submitted for listing EOIs by applicant name
 if(isset($_POST['list_by_applicant'])) {
-    $first_name = $_POST['first_name'];
-    $last_name = $_POST['last_name'];
+    $first_name = sanitise_input($_POST['first_name']);
+    $last_name = sanitise_input($_POST['last_name']);
     $sql = "SELECT * FROM EOI WHERE first_name = '$first_name' OR last_name = '$last_name'";
     $result = mysqli_query($conn, $sql);
     // Display the results
@@ -35,7 +42,7 @@ if(isset($_POST['list_by_applicant'])) {
 
 // Check if the form is submitted for deleting EOIs by job reference number
 if(isset($_POST['delete_by_reference'])) {
-    $job_del = $_POST['job_del'];
+    $job_del = sanitise_input($_POST['job_del']);
     $sql = "DELETE FROM EOI WHERE job_reference = '$job_del'";
     $delete = mysqli_query($conn, $sql);
     // Display success or failure message
@@ -67,7 +74,7 @@ if(isset($_POST['delete_by_reference'])) {
             <input type="submit" name="list_all" value="List" class="button">
         </form>
 
-        <form method="post" class="manage-form">
+        <form method="post" class="manage-form" novalidate='novalidate'>
             <h2>List by job reference</h2>
             <div class="input-field">
                 <input type="text" id="job_reference" name="job_reference" pattern="[A-Za-z0-9]{5}" required placeholder=" ">
@@ -76,14 +83,14 @@ if(isset($_POST['delete_by_reference'])) {
             <input type="submit" name="list_by_reference" value="List" class="button">
         </form>
 
-        <form method="post" class="manage-form">
+        <form method="post" class="manage-form" novalidate='novalidate'>
             <h2>List by applicant's name</h2>
             <div class="input-field">
-                <input type="text" id="first_name" name="first_name" pattern="[\p{L}\p{Mn}\p{Pd}'\s]{1,20}" placeholder=" ">
+                <input type="text" id="first_name" name="first_name" pattern="[\p{L}\p{Mn}\p{Pd}'\s]{1,20}" required placeholder=" ">
                 <span>First name</span>
             </div>
             <div class="input-field">
-                <input type="text" id="last_name" name="last_name" pattern="[\p{L}\p{Mn}\p{Pd}'\s]{1,20}" placeholder=" ">
+                <input type="text" id="last_name" name="last_name" pattern="[\p{L}\p{Mn}\p{Pd}'\s]{1,20}" required placeholder=" ">
                 <span>Last name</span>
             </div>
             <input type="submit" name="list_by_applicant" value="List" class="button">
